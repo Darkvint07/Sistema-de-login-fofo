f95d3fe61b84a5ec4665a71615aa8838d58135f2
Angelica Conegundes <angelicaconegundes4@gmail.com>
2025-10-10 09:23:16 -0300

    Corrigi o cadastro de usuários

--- SistemaLogin.java
+++ SistemaLogin.java
@@ -20,28 +20,28 @@ public class SistemaLogin {
 
 }
 
-// AGORA A PARTE DO CADASTRO:
+    // AGORA A PARTE DO CADASTRO:
+    public static boolean cadastrar(String usuario, String senha) {
+        try (Connection con = Conexao.conectar()) {
+            String sqlCheck = "SELECT 1 FROM usuarios WHERE usuario = ?";
+            PreparedStatement st = con.prepareStatement(sqlCheck);
+            st.setString(1, usuario);
+            ResultSet rs = st.executeQuery(); // Esta linha é obrigatória
+            if (rs.next()) {
+                return false; // usuário já existe
+            }
 
-public static boolean cadastrar(String usuario, String senha) {
-    try (Connection con = Conexao.conectar ()) {
-        String sqlCheck = "SELECT 1 FROM usuario WHERE usuario = ?";
-        PreparedStatement st = con.prepareStatement(sqlCheck);
-        st.setString(1, usuario);
-        if (rs.next()) {
-            return false; //usuario ja existe
+            // INSERE NOVO USUÁRIO
+            String sqlInsert = "INSERT INTO usuarios (usuario, senha) VALUES (?, ?)";
+            st = con.prepareStatement(sqlInsert);
+            st.setString(1, usuario);
+            st.setString(2, senha);
+            st.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         }
-
-        //INSERE NOVO USUARIOO
-        String sqlInsert = "INSERT INTO usuarios (usuario, senha) VALUES (?,?)"
-        st = con.prepareStatement(sqlInsert)
-        st.setString(1, usuario);
-        st.setString(2, senha);
-        st.executeUpdate();
-        return true;
-    } catch (Exception e) {
-        e.printStackTrace();
-        return false;
     }
-         
-    ]
+
 }
